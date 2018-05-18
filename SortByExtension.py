a9eb85795b5c0ee0bf6625bab45b36f5dcf56c0b
Federico <fumbrella@gmail.com>
2018-05-18 20:18:58 +0200

    done

--- SortByExtension.py
+++ SortByExtension.py
@@ -1,9 +1,26 @@
-per ogni (file.dir){
-	
-	check(.*)
-	if(.* exist)
-		move(cur.file, dir /.*)
-	else
-		create(dir /.*)
-		move(cur.file, dir /.*)
-	}
+import os, shutil
+
+def files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
+
+directory = './'
+
+for entry in files(directory):
+	# Getting the extension
+	print("File: " + entry)
+	extension = entry.split(".")[-1]
+	extension = extension.upper()
+	print("Extension: " + extension)
+
+	# Checking if the folder for that extension exists
+	if not os.path.exists(directory + extension):
+		os.makedirs(directory + extension)
+		print("Created folder for " + extension + " files")
+
+	src = (directory + entry)
+	dest = (directory + extension)
+	shutil.move(src, dest)
+
+	print("Moved " + entry + " to " + dest)
